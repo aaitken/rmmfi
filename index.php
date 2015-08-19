3ca8f65f79a8109c0c2119f1044d9ce2ce0e320c
Alex Aitken <aitken.alexander@gmail.com>
2015-08-19 13:00:27 -0600

    add updated .rp file and generated html for heroku test

--- index.php
+++ index.php
@@ -1,2 +1,4 @@
-//http://www.lemiffe.com/how-to-deploy-a-static-page-to-heroku-the-easy-way/
-<?php include_once("./html/index.html"); ?>
+<?php
+	//http://www.lemiffe.com/how-to-deploy-a-static-page-to-heroku-the-easy-way/
+	include_once("./html/index.html");
+?>
