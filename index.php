4fd44f7ed932454ffad368760e3418624a83f2ee
Alex Aitken <aitken.alexander@gmail.com>
2015-08-19 13:10:41 -0600

    change php include_once to redirect

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
 	//http://www.lemiffe.com/how-to-deploy-a-static-page-to-heroku-the-easy-way/
-	include_once("./html/index.html");
+	//http://stackoverflow.com/questions/768431/how-to-make-a-redirect-in-php
+	header( 'Location: http://rmmfi.herokuapp.com/html/index.html' );
+	die();
 ?>
